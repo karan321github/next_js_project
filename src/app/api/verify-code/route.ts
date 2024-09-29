2eb4c8096b50a905d2856ca4bca753a83a5a5d34
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-09-29 22:00:42 +0530

    Added route to verify the code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,62 @@
+import connectDB from "@/lib/connectDB";
+import UserModel from "@/models/user";
+
+export async function POST(request: Request) {
+  await connectDB();
+  try {
+    const { userName, code } = await request.json();
+    const decodedUsername = decodeURIComponent(userName);
+    const user = await UserModel.findOne({ username: decodedUsername });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const isValidCode = user.verifyCode === code;
+    const isCodeNotExpire = new Date(user.veryifyCodeExpiry) > new Date();
+    if (isValidCode && isCodeNotExpire) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        {
+          success: true,
+          message: "Account verified",
+        },
+        { status: 200 }
+      );
+    } else if (!isValidCode) {
+      return Response.json(
+        {
+          success: false,
+          message: "Verify code does not matched",
+        },
+        { status: 405 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verify code has been expired please signup again to get a new  code",
+        },
+        { status: 405 }
+      )
+    }
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in verifying user",
+      },
+      { status: 500 }
+    );
+  }
+}
