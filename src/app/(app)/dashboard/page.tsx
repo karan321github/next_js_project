e8d107c681ee7e9de572ca6a94327c5266df6ed0
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-10-23 23:46:04 +0530

    Added the styling for dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import MessageCard from "@/components/MessageCard";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 import { useToast } from "@/hooks/use-toast";
 import { Message, User } from "@/models/user";
 import { acceptMessageValidation } from "@/schemas/acceptMessageSchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios, { AxiosError } from "axios";
+import { Loader2, RefreshCcw } from "lucide-react";
 import { useSession } from "next-auth/react";
 import React, { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -123,7 +128,62 @@ const Dashboard = () => {
   if (!session || !session.user) {
     return <div>please login</div>;
   }
-  return <div>Dashboard</div>;
+  return (
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6-xl">
+      <h1 className="text-4xl font-semibold mb-4">User Dashboard</h1>
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy your unique link</h2>{" "}
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            title="User profile URL"
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipBoard}>Copy</Button>
+        </div>
+      </div>
+      <div className="mb-4">
+        <Switch
+          {...register("acceptMessage")}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+        <Separator />
+        <Button
+          className="mt-4"
+          variant="outline"
+          onClick={(e) => {
+            e.preventDefault();
+            fetchMessages(true);
+          }}
+        >
+          {isLoading ? (
+            <Loader2 className="h-4 w-4 animate-spin" />
+          ) : (
+            <RefreshCcw className="h-4 w-4" />
+          )}
+        </Button>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+          {messages.length > 0 ? (
+            messages.map((message, index) => (
+              <MessageCard
+                key={message._id as string}
+                message={message}
+                onMessageDelete={handleDeleteMessage}
+              />
+            ))
+          ) : (
+            <p>No message to display</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
